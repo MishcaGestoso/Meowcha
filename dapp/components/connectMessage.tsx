ab03b59066cbe3a5dcced9ef5bc777cf7e144836
MishcaGestoso <89111017+MishcaGestoso@users.noreply.github.com>
2024-03-03 08:33:23 +0000

    icon and fixed a position

--- dapp/components/connectMessage.tsx
+++ dapp/components/connectMessage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function ConnectMessage() {
   return (
-    <div className="mb-10 flex flex-col justify-center items-center text-center text-white text-4xl font-wonderbar h-full ">
+    <div className="mb-10 lg:mb-28 flex flex-col justify-center items-center text-center text-white text-4xl font-wonderbar h-full ">
       <p className="drop-shadow-md">CONNECT YOUR WALLET TO GET SOME SWEETS</p>
     </div>
   );
